98192f4c586fa0d3b18a8e29b2e98bc01ac892bf
MashrafiGit <mdmasrafi902@gmail.com>
2025-08-29 03:13:31 +0600

    insert into BST

---
+++ insert_into_a_BST.cpp
@@ -0,0 +1,32 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode
+    {
+    public:
+        T val;
+        TreeNode<T> *left, *right;
+        TreeNode() : val(0), left(NULL), right(NULL) {}
+        TreeNode(T x) : val(x), left(NULL), right(NULL) {}
+        TreeNode(T x, TreeNode<T> *left, TreeNode<T> *right) : val(x), left(left), right(right) {}
+    };
+
+
+************************************************************/
+// TreeNode<int>* insertionInBST(TreeNode<int>* root, int val)
+// {
+//     if (root == NULL) return new TreeNode<int>(val);
+
+//     if (root->val > val) {
+//         if (root->left == NULL) root->left = new TreeNode<int>(val);
+//         else root->left = insertionInBST(root->left, val);
+//     }
+//     else if (root->val < val) {
+//         if (root->right == NULL) root->right = new TreeNode<int>(val);
+//         else root->right = insertionInBST(root->right, val);
+//     }
+
+//     return root;
+// }
